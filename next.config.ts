b27a46669e6ddbc647120f21e59a7279ff2c4ed4
Ngô Hữu Nam <namnh@teracom.vn>
2026-03-09 02:05:24 +0700

    feat: add UI components for labels, navigation menus, popovers, selects, separators, skeletons, spinners, switches, tabs, textareas, and typography
    - Implemented Label component for form labeling.
    - Created NavigationMenu with subcomponents for navigation structure.
    - Added Popover component for displaying contextual information.
    - Developed Select component with various subcomponents for dropdown functionality.
    - Introduced Separator component for visual separation in layouts.
    - Added Skeleton component for loading placeholders.
    - Implemented Spinner component for loading indicators.
    - Created Switch component for toggle functionality.
    - Developed Tabs component for tabbed navigation.
    - Added Textarea component for multi-line text input.
    - Implemented Typography components for consistent text styling.
    
    feat: set up authentication feature with API, slice, and components
    
    - Created authentication API service.
    - Implemented authSlice for managing authentication state.
    - Developed SigninForm component for user sign-in.
    - Set up index file for authentication feature.
    - Defined types for authentication.
    
    feat: implement internationalization proxy and query client setup
    
    - Added proxy function for locale handling in requests.
    - Implemented getQueryClient function for React Query setup.
    
    fix: update tsconfig paths for module resolution
    
    - Changed path mapping in tsconfig.json to point to src directory.

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
 import type { NextConfig } from "next";
-
+import createNextIntlPlugin from "next-intl/plugin";
+const withNextIntl = createNextIntlPlugin();
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
@@ -13,4 +14,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withNextIntl(nextConfig);
